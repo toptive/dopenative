fba8aef8ff1149c316953f19736835973a6cac61
Florian Marcu <florian.mrc@gmail.com>
2021-11-10 12:04:41 -0800

    babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins:
+    process.env.NODE_ENV === 'production' ? ['transform-remove-console'] : [],
+}
