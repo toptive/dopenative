365c940b182a285a0979ef2d96fa344b8933fa70
Florian Marcu <florian.mrc@gmail.com>
2021-12-18 12:17:10 -0800

    useColorScheme from react native instead of 3rd party

--- src/theming/index.js
+++ src/theming/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import { useColorScheme } from 'react-native'
 import DNDefaultTheme from './default'
-import { useColorScheme } from 'react-native-appearance'
 
 export default DNDefaultTheme
 
